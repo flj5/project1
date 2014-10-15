ac4a8e0e363df7fedbefde43b6683abe51369231
flj5 <flj5@zips.uakron.edu>
2014-10-15 15:04:25 -0400

    Completed project

---
+++ flj5_project1/flj5_project1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  flj5_project1
+//
+//  Created by Johnson,Fred L on 10/15/14.
+//  Copyright (c) 2014 Johnson,Fred L. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
